fb3e550fa7a12005d8bdd008bc8c90b5680143e0
DMVerton <dverton@ismg.io>
2025-03-18 12:50:46 -0400

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,18 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:1337',
+        'https://8d587402-801c-49e8-aaed-c39af9c1ac30.lovableproject.com',
+        'https://contentlogix.ai'
+      ]
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
